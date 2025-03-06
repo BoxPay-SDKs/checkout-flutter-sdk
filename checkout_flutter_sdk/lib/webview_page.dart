0e2c6d121ca60755855e6ec5986194a894196fd6
Ishika1601-boxpay <ishika.bansal@boxpay.tech>
2025-03-06 11:29:51 +0530

    session expired handled

--- checkout_flutter_sdk/lib/webview_page.dart
+++ checkout_flutter_sdk/lib/webview_page.dart
@@ -370,45 +370,24 @@ class _WebViewPageState extends State<WebViewPage> {
           stopFunctionCalls();
         } else if (status?.toUpperCase().contains("PENDING")) {
         } else if (status?.toUpperCase().contains("EXPIRED")) {
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return Stack(
-                  children: <Widget>[
-                    Container(
-                      color: Colors.white,
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height,
-                    ),
-                    const Positioned(
-                      top: 0,
-                      left: 0,
-                      right: 0,
-                      child: CustomAppBar(title: 'Checkout'),
-                    ),
-                    AlertDialog(
-                      title: const Text("Payment Session Expired"),
-                      content: const Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("Please Click on EXIT to restart"),
-                        ],
-                      ),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text("Exit"),
-                        ),
-                      ],
-                    ),
-                  ],
-                );
-              });
-        } else if (status?.toUpperCase().contains("PROCESSING")) {
+          job?.cancel();
+          stopFunctionCalls();
+          redirectModal(
+            context,
+            title: "Payment Session Expired",
+            content: "Please exit checkout and restart",
+            yesButtonText: "Exit",
+            noButtonText: "",
+            onYesPressed: (Completer<bool> completer) async {
+              Navigator.of(context).pop();
+              completer.complete(false);
+              widget.onPaymentResult(
+                  PaymentResultObject("Expired", tokenFetched));
+            },
+            onNoPressed: (Completer<bool> completer) {
+              // no op
+            },
+          );
           tokenFetched = jsonResponse["transactionId"];
         } else if (status?.toUpperCase().contains("FAILED")) {
           tokenFetched = jsonResponse["transactionId"];
